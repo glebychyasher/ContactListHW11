f4d250e2670fccf7dbc0619b545605211992b05d
glebychyasher <zobningleb01@icloud.com>
2023-10-26 17:11:06 +0300

    Initial Commit

---
+++ ContactListHW11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListHW11
+//
+//  Created by Глеб Зобнин on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
